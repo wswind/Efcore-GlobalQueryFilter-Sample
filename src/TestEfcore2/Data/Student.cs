084abcb2564c61372e062d31c61e5cdb43dde828
wswind <18399096+wswind@users.noreply.github.com>
2019-11-27 16:54:40 +0800

    up

--- src/TestEfcore2/Data/Student.cs
+++ src/TestEfcore2/Data/Student.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        //public bool IsDeleted { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
